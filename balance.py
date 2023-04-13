2ca6edf69a1aae699e5391bc228c0468f5415e13
UnknownGhost09 <69518728+UnknownGhost09@users.noreply.github.com>
2023-04-13 10:59:01 +0530

    Create balance.py

---
+++ balance.py
@@ -0,0 +1,17 @@
+from binance.exceptions import BinanceAPIException
+from binance.client import Client
+#TESTNET
+
+#API_KEY='7gr7eBfTq9IqaexFkjRVje17t8dTLjE30fck2oVucTURRqOqlYULW8xnKh8rMiZR'
+#SECRET_KEY='Ai6F7MpVZvHQBHC0Yb07Djs5I2bmjUqsRVgssjPn3UzcviVn8VbSg5L3ZiC3pYjM'
+
+#ORIGINAL
+
+API_KEY = 'QSOAqPicgAdGuxFrUX3fRyliUKwZvMZz5W2vmozNIZrxUu4hUw5FIRkv1G2TO4F2'
+SECRET_KEY = 'vYvmMCee2XSKd5o4asykDoB8lR93XCSvAYYWRjA9Fk4tF0mOtEByijNEMX3TVyCl'
+
+client = Client(api_key=API_KEY,api_secret=SECRET_KEY,)
+
+
+balance = client.get_account()['balances']
+print(balance)
